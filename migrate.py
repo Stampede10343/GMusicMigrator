deddc8652eef1b927eb4df8a72113b20cc15fb3f
Cameron Cordes <cameronc.dev@gmail.com>
2020-08-20 13:14:41 -0400

    Fix input and track selection

--- migrate.py
+++ migrate.py
@@ -6,8 +6,9 @@ import os
 import sys
 import logging
 
-logging.basicConfig("output.log", level=logging.DEBUG)
+logging.basicConfig(filename="output.log", level=logging.DEBUG)
 LOG = logging.getLogger()
+LOG.setLevel(logging.DEBUG)
 
 def has_artist(artist, artists):
     cleaned = list(map(lambda art: art['name'], artists))
@@ -19,20 +20,49 @@ def save_track(saved_tracks, track):
 
 def pick_track(saved_tracks, tracks):
     sorted_tracks = list(sorted(tracks, key=lambda track: track['popularity']))
+    print("Please select a track:")
     for i, track in enumerate(sorted_tracks):
-        print("Please select a track:")
         print("{})".format(i), track['name'], track['artists'][0]['name'])
 
     while True:
-        selection = input("Select a track (or -1 to skip):")
-        if select == -1:
-            break
-        elif select >= 0 and select < len(sorted_tracks):
-            save_track(saved_tracks, sorted_tracks[selection])
-            break
+        selection = input("Select a track (or -1 to skip): ")
+        try:
+            selection = int(selection)
+            if selection == -1:
+                break
+            elif selection >= 0 and selection < len(sorted_tracks):
+                save_track(saved_tracks, sorted_tracks[selection])
+                break
+        except Exception as e:
+            pass
 
 def add_tracks_to_playlist(saved_tracks, playlist):
-    LOG.log("Adding {} songs to playlist: {}", len(saved_tracks), playlist)
+    print("Adding {} songs to playlist: {}", len(saved_tracks), playlist)
+    LOG.info("Adding %s songs to playlist: %s", len(saved_tracks), playlist)
+
+def find_song(line, saved_tracks):
+    artist_song = line.replace('\n', '').split(' - ')
+    artist = artist_song[0]
+    song = artist_song[1]
+
+    result = sp.search(artist + ' ' + song)
+    tracks = result['tracks']['items']
+    if len(tracks) == 0:
+        LOG.warning("Track not found: %s - %s".format(artist, song))
+    elif len(tracks) == 1:
+        LOG.debug("Ideal case")
+        save_track(saved_tracks, tracks[0])
+    else:
+        exact_matches = list(filter(lambda t: song in t['name'] and
+                                    has_artist(artist, t['artists']), tracks))
+        if len(exact_matches) == 0:
+            LOG.debug("No exact matches")
+            pick_track(saved_tracks, tracks)
+        elif len(exact_matches) == 1:
+            LOG.debug('One exact matching result')
+            save_track(saved_tracks, exact_matches[0])
+        else:
+            pick_track(saved_tracks, exact_matches)
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -43,7 +73,7 @@ auth = SpotifyOAuth(scope=scope,
                     client_id=os.getenv("SPOTIPY_CLIENT_ID"),
                     client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
                     redirect_uri="http://localhost")
-LOG.debug("Running with token: {}", auth.get_access_token())
+LOG.debug("Running with token: %s", auth.get_access_token())
 
 sp = spotipy.Spotify(auth_manager=auth)
 
@@ -51,30 +81,10 @@ for playlist in glob.glob('playlists/*'):
     saved_tracks = []
     with open(playlist, 'r') as file:
         for line in file:
-            artist_song = line.replace('\n', '').split(' - ')
-            artist = artist_song[0]
-            song = artist_song[1]
-
-            result = sp.search(artist + ' ' + song)
-            tracks = result['tracks']['items']
-            if len(tracks) == 0:
-                LOG.log("Track not found: {} - {}".format(artist, song))
-            elif len(tracks) == 1:
-                LOG.debug("Ideal case")
-            else:
-                exact_matches = list(filter(lambda t: song in t['name'] and
-                                            has_artist(artist, t['artists']), tracks))
-                if len(exact_matches) == 0:
-                    LOG.debug("No exact matchs")
-                    pick_tracks(saved_tracks, tracks)
-                elif len(exact_matches) == 1:
-                    LOG.debug('One exact matching result')
-                    save_track(saved_tracks, exact_matches[0])
-                else:
-                    pick_track(saved_tracks, exact_matches)
+            find_song(line, saved_tracks)
             print()
             break
 
-        add_tracks_to_playlist(saved_tracks, playlist)
+        add_tracks_to_playlist(saved_tracks, playlist.split('/')[1])
     pass
 
