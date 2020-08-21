5e4c8c0deee4d9f1933ed7174184ad3fed1d3983
Cameron Cordes <cameronc.dev@gmail.com>
2020-08-20 22:26:31 -0400

    Fix adding too many songs at once
    Improved track selection in simple cases.
    Added some basic error handling/logging.

--- migrate.py
+++ migrate.py
@@ -20,9 +20,16 @@ def save_track(saved_tracks, track):
 
 def pick_track(saved_tracks, tracks):
     sorted_tracks = list(sorted(tracks, key=lambda track: track['popularity'], reverse=True))
+    if len(sorted_tracks) < 3:
+        unique_tracks = set([ t['name'] + t['album']['name'] for t in sorted_tracks ])
+        if len(unique_tracks) == 1:
+            print("Found duplicate track")
+            save_track(saved_tracks, sorted_tracks[0])
+            return
+
     print("Please select a track:")
     for i, track in enumerate(sorted_tracks):
-        print("{}) {} - {} - {}".format(i, track['name'], list(map(lambda t: t['name'], track['artists'])), track['album']['name']))
+        print("{}) {} - {} - {}".format(i, track['name'], list(map(lambda a: a['name'], track['artists'])), track['album']['name']))
 
     while True:
         selection = input("Select a track (or -1 to skip): ")
@@ -35,6 +42,19 @@ def pick_track(saved_tracks, tracks):
                 break
         except Exception as e:
             pass
+    print()
+
+
+def chunk_playlist(user_id, playlist_id, track_ids):
+    size = 99
+    chunks = [ track_ids[i:i+size] for i in range(0, len(track_ids), size) ]
+    first_insert = True
+    for chunk in chunks:
+        if first_insert:
+            first_insert = False
+            sp.user_playlist_replace_tracks(user_id, playlist_id, chunk)
+        else:
+            sp.user_playlist_add_tracks(user_id, playlist_id, chunk)
 
 def add_tracks_to_playlist(saved_tracks, playlist):
     LOG.info("Adding %s songs to playlist: %s", len(saved_tracks), playlist)
@@ -45,18 +65,16 @@ def add_tracks_to_playlist(saved_tracks, playlist):
     if len(user_playlists) == 0:
         created_playlist = sp.user_playlist_create(user_id, playlist)
         playlist_id = created_playlist['id']
-        print(playlist_id)
-        sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids)
-        pass
+        chunk_playlist(user_id, playlist_id, track_ids)
     else:
         print(user_playlists)
         existing_playlists = list(filter(lambda p: playlist == p['name'], user_playlists))
         if len(existing_playlists) == 0:
             created_playlist = sp.user_playlist_create(user_id, playlist)
             playlist_id = created_playlist['id']
-            sp.user_playlist_replace_tracks(user_id, playlist_id, track_ids)
+            chunk_playlist(user_id, playlist_id, track_ids)
         elif len(existing_playlists) == 1:
-            sp.user_playlist_replace_tracks(user_id, existing_playlists[0]['id'], track_ids)
+            chunk_playlist(user_id, existing_playlists[0]['id'], track_ids)
         else:
             LOG.err("Multiple playsts with name exists?? %s", existing_playlists)
 
@@ -70,6 +88,7 @@ def find_song(line, saved_tracks):
     tracks = result['tracks']['items']
     if len(tracks) == 0:
         LOG.warning("Track not found: %s - %s", artist, song)
+        # Try searching with parens removed
     elif len(tracks) == 1:
         LOG.debug("Ideal case")
         save_track(saved_tracks, tracks[0])
@@ -85,6 +104,7 @@ def find_song(line, saved_tracks):
         else:
             pick_track(saved_tracks, exact_matches)
 
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -94,7 +114,7 @@ auth = SpotifyOAuth(scope=scope,
                     client_id=os.getenv("SPOTIPY_CLIENT_ID"),
                     client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
                     redirect_uri="http://localhost")
-LOG.debug("Running with token: %s", auth.get_access_token())
+print("Running with token: %s", auth.get_access_token())
 
 sp = spotipy.Spotify(auth_manager=auth)
 
@@ -102,9 +122,16 @@ for playlist in glob.glob('playlists/*'):
     saved_tracks = []
     with open(playlist, 'r') as file:
         for line in file:
-            find_song(line, saved_tracks)
-            print()
+            try:
+                find_song(line, saved_tracks)
+            except Exception as e:
+                print("Error adding track: {}", line)
+                LOG.error("Error adding track: %s", line, e)
 
-        add_tracks_to_playlist(saved_tracks, playlist.split('/')[1])
+        try:
+            add_tracks_to_playlist(saved_tracks, playlist.split('/')[1])
+        except Exception as e:
+            LOG.fatal("Error saving playlist: %s", playlist.split('/')[1], e)
+            raise e
     break
 
